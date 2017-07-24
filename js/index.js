a97e64a0f5cd041be25a40188c81d38b6da9112c
Bohdan Melenchuk <jailedkurtanogle@gmail.com>
2017-07-24 15:41:19 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const userName = prompt('Please enter your name');
+const helloMessage = 'Hello ' + userName;
+
+// alert(helloMessage);
+// document.write(helloMessage);
+// console.log(helloMessage);
+
+
+
+let age = Number(prompt('Enter your age'));
+
+if (isNaN(age)) {
+    document.write('Please enter correct number');
+    return;
+}
+
+{
+    if (age !== 40) 
+    
+}
+    {
+        document.write('You are young!');
+    
+    
+    
+        document.write('You are not young!');} 
